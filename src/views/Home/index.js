6a6298f1ac5ef2d521ae016fcbbb5d9aed6aa638
Valentina <valenam2106@gmail.com>
2023-04-21 16:52:25 -0500

    Deleted commets

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,25 +1,12 @@
 import { useEffect, useContext } from "react";
-//import shallow from "zustand/shallow";
+
 import PokemonContext from "../../context/pokemons";
 import PokemonList from "./components/PokemonList";
 import Loading from "../../components/Loading/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
-//import usePokemonsStore from "../../zustand/stores/pokemons";
 
 export default function Home() {
-    /*const {
-        getPokemons,
-        pokemons,
-        isLoading,
-        hasError,
-        errorMessage
-    } = usePokemonsStore(state => ({
-        getPokemons: state.getPokemons,
-        pokemons: state.pokemons,
-        isLoading: state.isLoading,
-        hasError: state.hasError,
-        errorMessage: state.errorMessage
-    }), shallow); // zustand*/
+
     const { getPokemons, pokemons, isLoading, hasError, errorMessage } = useContext(PokemonContext);
 
     useEffect(() => {
