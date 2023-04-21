753a90ea6281fe7e69cebc36dfde7f8cb0d256ca
Valentina <valenam2106@gmail.com>
2023-04-21 15:10:36 -0500

    UseLocation

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,13 +1,16 @@
 import { useContext, useEffect } from "react";
 import PokemonContext from "../../context/pokemons";
 import PokemonList from "./components/PokemonList";
+import Loading from "../../components/Loading/Loading";
 
 export default function Home() {
-    const { getPokemons, pokemons } = useContext(PokemonContext);
+    const { getPokemons, pokemons, isLoading } = useContext(PokemonContext);
 
     useEffect(() => {
         getPokemons().catch(null);
     }, []);
+
+    if (isLoading) return <Loading title="Cargando resultados..." />;
 
     return (<div>
         <PokemonList pokemons={pokemons} />
